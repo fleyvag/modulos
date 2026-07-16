3f07e93d2b938ab76259c710d4e032dd58a9bb48
fleyvag <43174467+fleyvag@users.noreply.github.com>
2026-07-15 20:37:24 -0500

    Add files via upload

--- sead_contacto_actividades/models/actividad_padre.py
+++ sead_contacto_actividades/models/actividad_padre.py
@@ -4,6 +4,5 @@ from odoo import fields, models
 class ActividadPadre(models.Model):
     _name = 'actividad.padre'
     _description = 'Actividad Padre'
-    _rec_name = 'actividad'
 
     actividad = fields.Char(string='Actividad', required=True)
